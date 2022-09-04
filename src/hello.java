3ba1de96bcc4de92c03ed98a06e866c8c05ef295
Frog <frogmeat55@gmail.com>
2022-09-04 20:13:35 +0900

    branches1 commit

--- src/hello.java
+++ src/hello.java
@@ -1,7 +1,5 @@
 public class hello {
     public static void main(String[] args ){
-        System.out.println("hello");
-        System.out.println("hello2");
         System.out.println("branches2");
     }
 }
